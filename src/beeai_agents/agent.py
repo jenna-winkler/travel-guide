da0387c7c8718c09f224facae4a32784310a705c
Tomas Weiss <tomas.weiss2@gmail.com>
2025-06-26 13:53:33 +0200

    chore: update to latest acp
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- src/beeai_agents/agent.py
+++ src/beeai_agents/agent.py
@@ -1,15 +1,20 @@
 import os
 from collections.abc import AsyncGenerator
 
-from acp_sdk import MessagePart, Metadata
+
+from acp_sdk import Annotations, MessagePart, Metadata
 from acp_sdk.models import Message
+from acp_sdk.models.platform import PlatformUIAnnotation, PlatformUIType
 from acp_sdk.server import Context, RunYield, RunYieldResume, Server
 
 server = Server()
 
-
 @server.agent(
-    metadata=Metadata(ui={"type": "hands-off"})
+    metadata=Metadata(
+        annotations=Annotations(
+            beeai_ui=PlatformUIAnnotation(ui_type=PlatformUIType.HANDSOFF)
+        )
+    )
 )
 async def example_agent(input: list[Message], context: Context) -> AsyncGenerator[RunYield, RunYieldResume]:
     """Polite agent that greets the user"""
